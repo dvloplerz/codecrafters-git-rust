95bc4c6f0ac4ebd52566add03809e2281704fd30
n01kno <ittipat135@gmail.com>
2024-01-16 23:11:09 +0700

    pass 2nd stage <Finally!>

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,27 @@ fn main() -> anyhow::Result<()> {
             fs::create_dir(".git")
                 .and_then(|_| fs::create_dir(".git/objects"))
                 .and_then(|_| fs::create_dir(".git/refs"))
-                .and_then(|_| fs::write(".git/HEAD", "ref: refs/heads/master\n"));
+                .and_then(|_| fs::write(".git/HEAD", "ref: refs/heads/master\n"))?;
             println!("Initialized git directory");
         }
         "cat-file" => {
-            let git_object_path = ".git/objects";
-            let mut raw_content = String::new();
-            let (path, file) = &args[2].split_at(2);
-            let file_location = Path::new(&git_object_path).join(path).join(file);
-            if file_location.exists() {
-                ZlibDecoder::new(File::open(file_location)?).read_to_string(&mut raw_content)?;
-                let prettyfy_content = raw_content.split_once("\0").unwrap().1;
-                println!("{}", prettyfy_content.trim())
-            };
+            if &args[2] == "-p" {
+                let git_object_path = ".git/objects";
+                let mut raw_content = String::new();
+                let (path, file) = &args[3].split_at(2);
+                let file_location = Path::new(&git_object_path).join(path).join(file);
+                if file_location.exists() {
+                    ZlibDecoder::new(File::open(file_location)?)
+                        .read_to_string(&mut raw_content)?;
+                    let prettyfy_content = raw_content
+                        .split_once("\0")
+                        .unwrap()
+                        .1
+                        .trim_end()
+                        .trim_end();
+                    print!("{}", prettyfy_content.trim_end())
+                };
+            }
         }
         _ => {
             println!("unknown command: {}", args[1])
